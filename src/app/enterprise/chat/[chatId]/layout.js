ea6e4f5b8d58405922f78b8adb409a11a40bf829
DanKamNdi <ndirangualvinkamau@gmail.com>
2024-11-15 11:08:47 +0300

    Fixed global css import in enterprise layout.js
    Signed-off-by: DanKamNdi <ndirangualvinkamau@gmail.com>

--- src/app/enterprise/chat/[chatId]/layout.js
+++ src/app/enterprise/chat/[chatId]/layout.js
@@ -1,7 +1,4 @@
-import {Inter} from 'next/font/google'
-import '../../globals.css'
-
-const inter = Inter({subsets: ['latin']})
+import '../../../globals.css'
 
 export default function ChatIdLayout({children}) {
   return (
